1293b7ec7a0d1850df359b4e9d76ab2d024b63dc
Deformator <dammandrii@gmail.com>
2017-11-29 19:48:19 -0500

    added FavouritesList.swift - utility class

---
+++ Week9/FavouritesList.swift
@@ -0,0 +1,39 @@
+
+import Foundation
+import UIKit
+
+class FavouritesList {
+    //privat variables
+    static let sharedFavouritesList = FavouritesList()
+    private(set) var favourites:[String]
+    
+    //constructor
+    init() {
+        let defaults = UserDefaults.standard
+        let storedFavourites = defaults.object(forKey: "favourites") as? [String]
+        favourites = storedFavourites != nil ? storedFavourites! : []
+    }
+    
+    //this method adds a font to the user preferences files
+    func addFavourite(fontName: String) {
+        if !favourites.contains(fontName){
+            favourites.append(fontName)
+            saveFavourites()
+        }
+    }
+    
+    //this method save font favourites to the user preferences file
+    private func saveFavourites() {
+        let defaults = UserDefaults.standard
+        defaults.set(favourites, forKey: "favourites")
+        defaults.synchronize()
+    }
+    
+    //this method removes a favourite from the user preferences file
+    func removeFavourite(fontName: String){
+        if let index = favourites.index(of: fontName){
+            favourites.remove(at: index)
+            saveFavourites()
+        }
+    }
+}
