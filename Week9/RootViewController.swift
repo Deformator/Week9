3afbe2b6477071b9c6d997bbf33ac27ce30770be
Deformator <dammandrii@gmail.com>
2017-11-29 20:43:26 -0500

    modify TableViewController

---
+++ Week9/RootViewController.swift
@@ -0,0 +1,54 @@
+
+import UIKit
+
+class RootViewController: UITableViewController {
+    
+    //private instance variables
+    private var familyNames : [String]!
+    private var cellPointSize : CGFloat!
+    private var favouritesList: FavouritesList!
+    private static let familyCell = "FamilyName"
+    private static let favouritesCell = "Favourites"
+
+    //this event triggers the first time the view is going to be rendered
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    //this event triggers the first time the application is loaded
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        familyNames = (UIFont.familyNames as [String]).sorted()
+        let preferedTableViewFont = UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
+        cellPointSize = preferedTableViewFont.pointSize
+        favouritesList = FavouritesList.sharedFavouritesList
+        tableView.estimatedRowHeight = cellPointSize
+    }
+    
+    //utility method
+    func fontForDisplay(atIndexPath indexPath: NSIndexPath) -> UIFont? {
+        if indexPath.section == 0 {
+            let familyName = familyNames[indexPath.row]
+            let fontName = UIFont.fontNames(forFamilyName: familyName).first
+            return fontName != nil ? UIFont(name: fontName!, size: cellPointSize) : nil
+        } else {
+            return nil
+        }
+    }
+    
+    //data source methods
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return favouritesList.favourites.isEmpty ? 1 : 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? familyNames.count : 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return section == 0 ? "All Font Families" : "My Favourite Fonts"
+    }
+
+}
